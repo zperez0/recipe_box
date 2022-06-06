102041559c3e25f5fe4ff82fe13d3e64605935c4
Yazmin Perez <zoeperez333@gmail.com>
2022-06-06 13:37:13 -0700

    [WIP] add recipe controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,57 @@
+class RecipesController < ApplicationController
+
+  # WIP: add tags
+
+  def index
+    @recipes = Recipe.all
+    render :index
+  end
+
+  def new
+    @recipe = Recipe.new
+    render :new
+  end
+
+  def create
+    @recipe = Recipe.new(recipes_params)
+    if @recipe.save
+      flash[:notice] = "Recipe successfully added!"
+      redirect_to products_path
+    else
+      flash[:alert] = "Oops..Looks like there was an error in creating your recipe. Try again."
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render :show
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipes_params)
+      flash[:notice]= "Recipe successfully updated"
+      redirect_to recipes_path
+    else
+      flash[:alert] = "Oops..Looks like there was an error in updating your recipe. Try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+  def recipes_params
+    params.require(:recipe).permit(:title, :ingredient, :instruction, :content_body, :rating )
+  end
+end
