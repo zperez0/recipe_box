796c446bce497a40d8b15b0fedea711a917bef98
Yazmin Perez <zoeperez333@gmail.com>
2022-06-07 16:29:29 -0700

    add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_214944) do
+ActiveRecord::Schema.define(version: 2022_06_07_232706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_06_07_214944) do
     t.string "email"
     t.string "password_hash"
     t.string "password_salt"
+    t.boolean "admin"
   end
 
 end
