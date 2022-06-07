1c20c424e807a16ceef759cc95147da5b897b244
Yazmin Perez <zoeperez333@gmail.com>
2022-06-07 15:20:16 -0700

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   root to: 'recipes#index'
   resources :recipes do
     resources :tags
+
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+  
+    get '/signin' => 'sessions#new'
+    post '/signin' => 'sessions#create'
+    get '/signout' => 'sessions#destroy'
   end
 end
