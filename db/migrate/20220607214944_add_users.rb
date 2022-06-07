904524de1d2c7433538e22ed90f1d8d5004dcfc2
Yazmin Perez <zoeperez333@gmail.com>
2022-06-07 14:54:01 -0700

    add users migration

---
+++ db/migrate/20220607214944_add_users.rb
@@ -0,0 +1,9 @@
+class AddUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_hash
+      t.string :password_salt
+    end
+  end
+end
