330ed2d75ef4f4acb28ac862c1b478202549a37f
Yazmin Perez <zoeperez333@gmail.com>
2022-06-08 14:17:34 -0700

    add user and session routes. change root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'recipes#index'
+  root to: 'recipes#home'
   resources :recipes do
     resources :tags
-
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
-  
-    get '/signin' => 'sessions#new'
-    post '/signin' => 'sessions#create'
-    get '/signout' => 'sessions#destroy'
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
