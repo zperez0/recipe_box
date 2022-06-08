999fb2287c8021cdcbeca0bae2499a265b16ab27
Yazmin Perez <zoeperez333@gmail.com>
2022-06-08 14:21:04 -0700

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   attr_accessor :password
-  validates_confirmation_od :password
-  validates :email, presence: true, uniqueness: true
+  validates_confirmation_of :password
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
   before_save :encrypt_password
 
   def encrypt_password
-    self.password_hash = BCrypt::Engine.generate_salt
-    self.password_salt = BCrypt::Engine.hash_secret(password, password_salt)
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password,password_salt)
   end
 
-  def self.authticate(email, password)
+  def self.authenticate(email, password)
     user = User.find_by "email = ?", email
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
